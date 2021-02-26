19a3bfa783e96657acb04a1991e7aa13a344ae37
Alexandr <ror_developer_3@itsites.com.ua>
2021-02-26 23:49:50 +0200

    learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,12 @@ class Comment < ApplicationRecord
 
   belongs_to :article
 
+  scope :gavno_name, -> { where(commenter: "gavno") }
+  scope :gavno_name_and_777, -> { gavno_name.where("body = 777") }
+
+  scope :recent, -> { where('created_at >= ?', Date.current.year - 50 )}
+  scope :old, -> { where('created_at < ?', Date.current.year - 50 )}
+
   # validates :commenter, presence: true, confirmation: { case_sensitive: false }, length: { minimum: 3 }
   # validates :commenter, confirmation: true
   # validates :commenter_confirmation, presence: true
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,18 @@
 class Product < ApplicationRecord
+
   has_many :pictures, as: :imageable
+
+  scope :costs_more_than, ->(amount) { where("price > ?", amount) if amount.present? }
+  # default_scope { where(description: nil) }
+
+  enum status: [:shipped, :being_packaged, :complete, :cancelled]
+
+  # def self.costs_more_than(amount)
+  #   where("price > ?", amount) if amount.present?
+  # end
+
+  def i_am_name
+    "I am #{name}"
+  end
+
 end
