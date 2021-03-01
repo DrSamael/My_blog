01189cf7bc6aaef75de4a9bed1bb442e0759c474
Alexandr <ror_developer_3@itsites.com.ua>
2021-03-01 23:09:29 +0200

    learn activemodel

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,30 @@
-class Employee < ApplicationRecord
-  has_many :pictures, as: :imageable
+class Employee# < ApplicationRecord
+  # has_many :pictures, as: :imageable
+
+  include ActiveModel::Dirty
+  define_attribute_methods :first_name, :last_name
+
+  def first_name
+    @first_name
+  end
+
+  def first_name=(value)
+    first_name_will_change!
+    @first_name = value
+  end
+
+  def last_name
+    @last_name
+  end
+
+  def last_name=(value)
+    last_name_will_change!
+    @last_name = value
+  end
+
+  def save
+    # метод для сохранения изменений...
+    changes_applied
+  end
+
 end
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,2 +1,20 @@
-class Vehicle < ApplicationRecord
+class Vehicle# < ApplicationRecord
+  include ActiveModel::AttributeMethods
+
+  attribute_method_prefix 'reset_'
+  attribute_method_suffix '_highest?'
+  define_attribute_methods 'price'
+
+  attr_accessor :price
+
+  private
+  def reset_attribute(attribute)
+    send("#{attribute}=", 0)
+  end
+
+  def attribute_highest?(attribute)
+    send(attribute) > 10
+  end
+
+
 end
