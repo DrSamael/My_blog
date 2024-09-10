450fd7e5086f252abd5e2bbf044092681c0915a5
Alexandr <oleksandr.savchuk@lasoft.org>
2024-09-10 14:01:05 +0300

    update unit tests for Article model

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  describe Article do
-    it 'has valid article' do
-      # use create, just FactoryGirl(:article) is deprecated now
-      expect(FactoryGirl.create(:article)).to be_valid
-    end
+  it { is_expected.to respond_to(:text) }
+  it 'has valid article' do
+    # use create, just FactoryGirl(:article) is deprecated now
+    expect(FactoryGirl.create(:article)).to be_valid
   end
 end
